a261ceb98815fbae0f0704aa03fa6d6790298df5
Daniel Wabyick <dwabyick@adobe.com>
2014-03-12 23:47:57 -0700

    removed boilerplate test

--- test/test.indentify.js
+++ test/test.indentify.js
@@ -11,13 +11,4 @@ describe('indentify', function() {
   })
 })
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-
-      it('should appropriately correct line indentation', function(){
-          expect([1,2,3].indexOf(5)).to.equal(-1);
-      })
-  })
-})
-
 
